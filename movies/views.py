de5478d94f94ada1cbb42aab907bf1df47e7ca78
Durga Sumesh <durgasumesh17@gmail.com>
2024-05-28 16:36:06 +0530

    add edit, delete functionality

--- movies/views.py
+++ movies/views.py
@@ -20,5 +20,20 @@ def list(request):
     print(movie_set)
     return render(request, 'list.html', {'movies' : movie_set})
 
-def edit(request):
-    return render(request, 'edit.html')
+def edit(request, pk):
+    movie_set = MovieInfo.objects.all()
+    instance_to_edit = MovieInfo.objects.get(pk=pk)
+    if request.POST:
+        frm = MovieForm(request.POST, instance = instance_to_edit)
+        if frm.is_valid():
+            instance_to_edit.save()
+            return render(request, 'list.html', {'movies' : movie_set})
+    frm = MovieForm(instance = instance_to_edit)
+    return render(request, 'create.html', {'frm' : frm})
+
+def delete(request, pk):
+    instance = MovieInfo.objects.get(pk=pk)
+    instance.delete()
+    movie_set = MovieInfo.objects.all()
+
+    return render(request, 'list.html', {'movies' : movie_set})
